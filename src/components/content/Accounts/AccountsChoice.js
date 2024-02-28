944146d299818bd4abdefb7226a20e6a9fa7e75c
Egor Martynov <b9955047856@gmail.com>
2024-02-28 21:38:30 +0300

    fix: account design, and currency info

--- src/components/content/Accounts/AccountsChoice.js
+++ src/components/content/Accounts/AccountsChoice.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import styles from "./AccountsChoice.module.css";
 
 const isAccount = [
@@ -31,8 +31,8 @@ const isAccount = [
     lastPaymentDate: "2024-02-26",
     rate: 15,
     payment: 0,
-    accountSumAmount: 50000,
-    depositAmount: 50000,
+    accountSumAmount: 4000,
+    depositAmount: 4000,
     lastPayment: 0,
     createDT: "2024-02-26T21:18:44.9687967",
     lastUpdateDT: "2024-02-26T21:18:44.9687967",
@@ -41,6 +41,8 @@ const isAccount = [
 ];
 
 const AccountsChoice = () => {
+  const [initCurrency, setInitCurrency] = useState();
+
   const formatedNumber = (num, currency) => {
     return num.toLocaleString("ru-RU", {
       style: "currency",
@@ -48,22 +50,36 @@ const AccountsChoice = () => {
     });
   };
 
+  const getRubAbbount = (currency, amount) => {
+    fetch("https://www.cbr-xml-daily.ru/daily_json.js")
+      .then((res) => res.json())
+      .then((data) => {
+        setInitCurrency(data["Valute"][currency]["Value"]);
+      })
+      .catch((error) => console.log(error));
+    return <p>{formatedNumber(initCurrency * amount, "RUB")}</p>;
+  };
+
   return (
     <Fragment>
       {isAccount.map((account) => (
         <div className={styles["node-info"]} key={account.id}>
-          <div>
-            <h4>{account.accountName}</h4>
-            <div className={styles.infos}>
-              <p>{account.rate} %</p>
-              <p>01.01.2023</p>
+          <h4>{account.accountName}</h4>
+          <div className={styles["node-info-add"]}>
+            <div>
+              <div className={styles.infos}>
+                <p>{account.rate} %</p>
+                <p>01.01.2023</p>
+              </div>
             </div>
-          </div>
-          <div className={styles.sum_info}>
-            <p className={`${styles["acc-sum"]} ${styles[account.type]}`}>
-              {formatedNumber(account.accountSumAmount, account.currency)}
-            </p>
-            <p className={styles["last-operation"]}>+ 125</p>
+            <div className={styles.sum_info}>
+              <p className={`${styles["acc-sum"]} ${styles[account.type]}`}>
+                {formatedNumber(account.accountSumAmount, account.currency)}
+              </p>
+              {account.currency != "RUB" &&
+                getRubAbbount(account.currency, account.accountSumAmount)}
+              <p className={styles["last-operation"]}>+ 125</p>
+            </div>
           </div>
         </div>
       ))}
